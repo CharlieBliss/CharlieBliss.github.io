73bf0fcc37eb5e7b86b2d792c7ec1e667ffb37cc
CharlieBliss <charles.m.bliss@gmail.com>
2016-05-04 14:28:31 -0400

    Move files from other repo

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+// $( document ).ready(function() {
+//     console.log( "ready!" );
+
+
+//   $('.project img').mouseenter(function() {
+//     $(this).css("cursor","pointer");
+//     $(this).animate({width: "21%", height: "21%"}, 'slow');
+//     $('img').css("background-color",'red');
+
+// });
+
+// $('.project img').mouseleave(function() {
+//     $(this).animate({width: "20%"}, 'slow');
+
+// });
+
+// });
